09e3f7e71177259e9715f4e43f4cc3d43f700bd2
Max Snegirev <goshasnegir@icloud.com>
2021-12-05 16:08:24 +0500

    Рефакторинг

--- postgres_to_es/load_data.py
+++ postgres_to_es/load_data.py
@@ -8,15 +8,18 @@ from postgres_loader import PostgresData
 from state import State, JsonFileStorage
 
 
-def main(pg_connection: _connection, state: State, es: ElasticsearchLoader) -> None:
+def main(pg_connection: _connection) -> None:
+    state = State(JsonFileStorage("PostgresStorages.json"))
     db_data = PostgresData(pg_connection, state).get_data()  # Получаем данные из базы
+
+    es = ElasticsearchLoader(conf.es_dsl, 'movies', 'es_schema/index.json', state)
+    es.create_index()
     es.load_es_data(db_data)  # Загружаем данные в Elasticsearch
 
 
 if __name__ == '__main__':
-    with psycopg2.connect(**conf.db_dsl, cursor_factory=DictCursor) as pg_conn:
-        state = State(JsonFileStorage("PostgresStorages.json"))
-        es = ElasticsearchLoader(conf.es_dsl, 'movies', 'es_schema/index.json', state)
-        es.create_index()
-
-        main(pg_conn, state, es)
+    try:
+        with psycopg2.connect(**conf.db_dsl, cursor_factory=DictCursor) as pg_conn:
+            main(pg_conn)
+    finally:
+        pg_conn.close()
